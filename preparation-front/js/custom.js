94d84fe30a95bebe5ea471f6b8d39b0a784a056e
julien camara <juliencamara9@gmail.com>
2024-05-14 23:13:58 +0000

    Rendre le slider de la page d'accuile auto-défilable

---
+++ preparation-front/js/custom.js
@@ -0,0 +1,9 @@
+
+
+// faire défiler les éléments du carousel sur la page d'accueil
+$(document).ready(function(){
+    // Déclenche le carousel toutes les 3 secondes
+    $('#header-carousel').carousel({
+        interval: 3000
+    });
+});
